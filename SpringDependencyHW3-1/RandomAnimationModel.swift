7085202c85030ae911c436df033292cc2f0703e5
ilyakozhevn <kozhevilya@gmail.com>
2022-08-04 20:00:45 +0300

    setup

---
+++ SpringDependencyHW3-1/RandomAnimationModel.swift
@@ -0,0 +1,16 @@
+//
+//  RandomAnimationModel.swift
+//  SpringDependencyHW3-1
+//
+//  Created by Ilya Kozhevnikov on 04.08.2022.
+//
+
+import UIKit
+import SpringAnimation
+
+struct randomAnimation {
+    let animation: AnimationPreset
+    let curve: AnimationCurve
+    
+    
+}
