0ef457e2b2136ec21be541d878939bd9ea462dce
ilyakozhevn <kozhevilya@gmail.com>
2022-08-04 19:39:06 +0300

    Initial Commit

---
+++ SpringDependencyHW3-1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpringDependencyHW3-1
+//
+//  Created by Ilya Kozhevnikov on 04.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
